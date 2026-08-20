0ae025d4d123b56982e1b77a7b5e55244a8931d4
chwa1679 <christopher.warth@web.de>
2026-08-20 20:26:22 +0200

    fix: configure TypeScript support for Cypress

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,10 +1,1 @@
 import 'cypress-axe';
-
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      injectAxe(): Chainable<void>;
-      checkA11y(context?: string, options?: object): Chainable<void>;
-    }
-  }
-}
